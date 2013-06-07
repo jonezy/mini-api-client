bab7a1b700741de18290f4e10e271f7aaca28c12
Chris Jones <chris@jonezy.org>
2013-06-07 11:41:49 -0400

    have the basic structure done, just need to implement all the endpoints
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 app.set('view options', { layout: false });
+app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
